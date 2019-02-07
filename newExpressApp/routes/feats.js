601849bfa6fda87bf94c5fc484d2a107baed2540
Max Watkins <willmwatkins@gmail.com>
2019-02-07 23:20:19 +1100

    now works with custom index and responsive api

--- newExpressApp/routes/feats.js
+++ newExpressApp/routes/feats.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let body = JSON.stringify(req.body);
+  res.send("REQ " + body);
 });
 
 module.exports = router;
