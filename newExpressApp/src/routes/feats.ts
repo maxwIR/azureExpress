1ce74949fa43b1836790a262698e3090e97925ac
Max Watkins <maxw@ir.com>
2019-02-26 10:22:09 +1100

    added an endpoint for searching

--- newExpressApp/src/routes/feats.ts
+++ newExpressApp/src/routes/feats.ts
@@ -1,6 +1,9 @@
 import { Router, Request, Response } from 'express';
 
 const router: Router = Router();
+router.get('/search', (req: Request, res: Response) => {
+   res.send(`Searched for ${JSON.stringify(req.body)}`);
+});
 router.use((req: Request, res: Response) => {
    res.sendFile('feats.json', {root: __dirname+'/../public'});
 });
