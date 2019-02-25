2c30569893fb3102627dc7c7456e4b62f2f280d4
Max Watkins <maxw@ir.com>
2019-02-25 17:03:14 +1100

    app config and new app runs the full thing

--- newExpressApp/src/app.ts
+++ newExpressApp/src/app.ts
@@ -2,9 +2,11 @@ import createError from 'http-errors';
 import express from 'express';
 import * as path from 'path';
 import cookieParser from 'cookie-parser';
+import { createServer } from "http";
 
 import { IndexRouter } from "./routes/index";
 import { FeatsRouter } from "./routes/feats";
+
 
 var app: express.Application = express();
 
@@ -18,18 +20,21 @@ app.use('/feats', FeatsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+   next(createError(404));
 });
 
 // error handler
 app.use(function(err: any, req: any, res: any, next: any) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+   // set locals, only providing error in development
+   res.locals.message = err.message;
+   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+   // render the error page
+   res.status(err.status || 500);
+   res.render('error');
 });
+
+const serv = createServer(app);
+serv.listen(process.env.PORT || '8080');
 
 module.exports = app;
