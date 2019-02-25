1bd815b76cc233c8b34a11115a8e9cb32f0fd395
Max Watkins <maxw@ir.com>
2019-02-26 09:16:28 +1100

    now working on azure. tweaking for desired routing

--- newExpressApp/src/app.ts
+++ newExpressApp/src/app.ts
@@ -14,9 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname, 'public')));
-
-app.use('/', IndexRouter);
 app.use('/feats', FeatsRouter);
+app.use('/', IndexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -24,15 +23,15 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err: any, req: any, res: any, next: any) {
-   // set locals, only providing error in development
-   res.locals.message = err.message;
-   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err: any, req: any, res: any, next: any) {
+//    // set locals, only providing error in development
+//    res.locals.message = err.message;
+//    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-   // render the error page
-   res.status(err.status || 500);
-   res.render('error');
-});
+//    // render the error page
+//    res.status(err.status || 500);
+//    res.render('error');
+// });
 
 const serv = createServer(app);
 serv.listen(process.env.PORT || '8080');
--- newExpressApp/src/routes/feats.ts
+++ newExpressApp/src/routes/feats.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from 'express';
 
 const router: Router = Router();
-router.use('/', (req: Request, res: Response) => {
+router.use((req: Request, res: Response) => {
     console.log('here');
       res.send("Made it");
 });
